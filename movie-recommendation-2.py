ac49473adbc37504f2f2b2f70e357046ec154abc
CLacaile <clementlacaile@gmail.com>
2019-06-03 13:05:02 +0200

    added console prints elements so when the stdErr is redirected, we can have a sense of what is happening

--- movie-recommendation-2.py
+++ movie-recommendation-2.py
@@ -1,5 +1,5 @@
 import numpy
-
+import time
 from pyspark.sql import SparkSession
 from pyspark.sql import Row
 from pyspark.ml.recommendation import ALS
@@ -12,31 +12,38 @@ def parseInput(line):
 
 if __name__ == "__main__":
 	# Build new session or retrieve existing one
+	print("Creating session...")
 	spark = SparkSession.builder.appName("movielens-recommender-2").getOrCreate()
 	
 	ml_small_path = "data/ml-latest-small/ratings.csv"
 	ml_path = "data/ml-latest/ratings.csv"
 	
-	print("Loading data from " + ml_small_path)
+	print("Loading data from " + ml_small_path + "...")
 	dataRDD = spark.read.text(ml_small_path).rdd
 	dataRDD_header = dataRDD.take(1)[0]
-	dataRDD_filtered = dataRR.filter(lambda l: l!=dataRDD_header)
+	dataRDD_filtered = dataRDD.filter(lambda l: l!=dataRDD_header)
 	ratingsRDD = dataRDD_filtered.map(parseInput)
 	ratings = spark.createDataFrame(ratingsRDD).cache()
+	print("Data loaded!")
 	
-	'''# Split data so 'training' is used to train the model 
-	# and 'test' is a subset where the model will be applied
-	(training, test) = ratings.randomSplit([0.8, 0.2])'''
-		
+	print("Training the model...")
+	start_train = time.time()
 	# Build the recommendation model using Alternating Least Squares
 	als = ALS(maxIter=5, regParam=0.01, userCol="userId", itemCol="movieId", ratingCol="rating",
 		coldStartStrategy="drop")
 	model = als.fit(ratings)
+	end_train = time.time()
+	print("Model trained in " + str(end_train - start_train) + "s!")
 	
 	# Build predictions on 'test' subset of 'ratings'
+	print("Building predictions...")
+	start_pred = time.time()
 	predictions = model.transform(ratings)
+	end_pred = time.time()
+	print("Predictions builded in " + str(end_pred - start_pred) + "s!")
 	
 	# Evaluate the model on training data
+	print("Evaluating the model...")
 	evaluator = RegressionEvaluator(metricName="rmse", labelCol="rating",
 		predictionCol="prediction")
 	rmse = evaluator.evaluate(predictions)
