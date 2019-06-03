99864cf9a4e7cd702383808140200a6cbc332ffe
CLacaile <clementlacaile@gmail.com>
2019-06-03 17:24:43 +0200

    added .master(local[*]) to the SparkSession builder to improve performance

--- movie-recommendation-2.py
+++ movie-recommendation-2.py
@@ -13,13 +13,14 @@ def parseInput(line):
 if __name__ == "__main__":
 	# Build new session or retrieve existing one
 	print("Creating session...")
-	spark = SparkSession.builder.appName("movielens-recommender-2").getOrCreate()
+	spark = SparkSession.builder\
+		.master("local[*]")
+		.appName("movielens-recommender-2").getOrCreate()
 	
 	ml_small_path = "data/ml-latest-small/ratings.csv"
-	ml_path = "data/ml-latest/ratings.csv"
 	
 	print("Loading data from " + ml_small_path + "...")
-	dataRDD = spark.read.text(ml_small_path).rdd
+	dataRDD = spark.read.text(ml_path).rdd
 	dataRDD_header = dataRDD.take(1)[0]
 	dataRDD_filtered = dataRDD.filter(lambda l: l!=dataRDD_header)
 	ratingsRDD = dataRDD_filtered.map(parseInput)
