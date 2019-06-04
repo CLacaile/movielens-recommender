97ba61e708bd6dbdf8aa2d7150749cb27b512d48
CLacaile <clementlacaile@gmail.com>
2019-06-04 09:47:59 +0200

    added userId=1 top 3 recommendations

--- movie-recommendation-2.py
+++ movie-recommendation-2.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
 	# Build the recommendation model using Alternating Least Squares
 	rank = 10
 	iterations = 10
-	als = ALS(userCol="userId", itemCol="movieId", ratingCol="rating", rank=100, regParam=0.01, nonnegative=False)
+	als = ALS(userCol="userId", itemCol="movieId", ratingCol="rating", rank=50, regParam=0.01, nonnegative=False)
 	model = als.fit(ratings)
 	end_train = time.time()
 	print("Model trained in " + str(end_train - start_train) + "s!")
@@ -44,6 +44,11 @@ if __name__ == "__main__":
 	predictions = model.transform(ratings)
 	end_pred = time.time()
 	print("Predictions builded in " + str(end_pred - start_pred) + "s!")
+	
+	# Recommend 3 movies for userId=1
+	user1_subset = ratings.where(ratings.userId==1)
+	user1_recomm = model.recommendForUserSubset(user1_subset, 3)
+	print(user1_recomm.take(3))
 	
 	# Evaluate the model on training data
 	print("Evaluating the model...")
